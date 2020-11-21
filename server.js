6f273707650a9fa819a336df56f3361099ade410
Adriana Ceron <adrianaceron@Adreana.home>
2020-11-21 14:41:22 +0100

    Level 1

--- server.js
+++ server.js
@@ -1,23 +1,56 @@
 const express = require("express");
+const bodyParser = require("body-parser")
 const cors = require("cors");
+const welcomeMessage = require("./welcomeMessage.json");
+const fileSys = require("body-parser")
+const fs = require("fs");
 
 const app = express();
 
 app.use(cors());
-
-const welcomeMessage = {
-  id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
-};
+app.use(bodyParser.json());
 
 //This array is our "data store".
 //We will start with one message in the array.
 //Note: messages will be lost when Glitch restarts our server.
-const messages = [welcomeMessage];
+
 
 app.get("/", function (request, response) {
   response.sendFile(__dirname + "/index.html");
 });
 
-app.listen(process.env.PORT);
+
+app.get("/welcome", function (request, response) {
+  response.send(welcomeMessage);
+});
+
+app.post("/welcome/post", function (request, response) {
+  const postContent = request.body;
+  const filterMessage = welcomeMessage.filter((el) => el.id === postContent.id)
+  const newMessage = welcomeMessage;
+  newMessage.push(postContent);
+  fs.writeFileSync("./welcomeMessage.json", JSON.striy(newMessage));
+  console.log(newMessage);
+  response.send(postContent);
+
+});
+
+/*/app.delete("/welcome/delete/:id", (req, res) => {
+  const id = req.params.id;
+  const filteredMessage = welcomeMessage.filter(message => message.id !== id);
+  res.send(filteredMessage);
+});/*/
+
+
+
+// app.get("/welcome/:id", function (request, response) {
+//const id = request.params.id;
+
+//});
+
+app.listen(3001, () => {
+  console.log("escuchando en el puerto 3001")
+});
+
+
+
